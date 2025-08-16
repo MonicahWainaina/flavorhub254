322b0ed5e3d1605382a1c5549a76b316008d964c
Monicah Wainaina <gwenmoni097@gmail.com>
2025-08-16 12:46:19 +0300

    Style:changed the recipe card design

--- flavorhub254/src/app/browse/page.js
+++ flavorhub254/src/app/browse/page.js
@@ -501,7 +501,7 @@ export default function BrowsePage() {
 						</div>
 					</div>
 					{/* Grid of Recipe Cards */}
-					<div className="grid grid-cols-1 sm:grid-cols-3 gap-6">
+					<div className="grid grid-cols-2 sm:grid-cols-3 gap-4 sm:gap-6">
 						{paginatedRecipes.map((recipe, idx) => {
 							const globalIdx = recipePage * recipesPerPage + idx;
 							const isFav = favoriteStates[globalIdx];
