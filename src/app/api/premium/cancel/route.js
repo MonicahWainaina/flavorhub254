66772a2ed150a2b3eab35cdb2253a2ea1f3f7363
Monicah Wainaina <gwenmoni097@gmail.com>
2025-10-11 22:40:20 +0300

    vercel: add FIREBASE_PRIVATE_KEY_BASE64

--- src/app/api/premium/cancel/route.js
+++ src/app/api/premium/cancel/route.js
@@ -37,7 +37,7 @@ if (!global._firebaseAdminInitialized) {
     type: "service_account",
     project_id: process.env.GOOGLE_PROJECT_ID,
     private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
-    private_key,
+    private_key: privateKey,
     client_email: process.env.GOOGLE_CLIENT_EMAIL,
     client_id: process.env.GOOGLE_CLIENT_ID,
     auth_uri: process.env.GOOGLE_AUTH_URI,
